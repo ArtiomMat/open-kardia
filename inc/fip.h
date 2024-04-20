54c25f6de48ece9c74c4ce5f3bb84982ba434013
doomguy <artiomat13100@gmail.com>
2024-04-20 14:06:19 +0300

    Added protection from double evaluation in fip.

--- inc/fip.h
+++ inc/fip.h
@@ -2,6 +2,8 @@
 // Fixed point arithmetic HEADER-ONLY module.
 // Entirely defined through macros, which allows one to dynamically change the bits through their source files
 
+// NOTE TO SELF: AVOID DOUBLE EVALUATION!!!!!
+
 #pragma once
 
 #define FIP_DEF_FRAC_BITS 10
@@ -14,11 +16,21 @@
 typedef int fip_t;
 
 #define fip_mul(a,b) (((fip_t)(a)*(fip_t)(b)) >> FIP_FRAC_BITS)
+
 #define fip_div(A,B) ((fip_t)((((long long)(A)) << FIP_FRAC_BITS) / ((long long)(B))))
+
 #define fip_frac(fip) ((fip_t)(fip) & ((1 << FIP_FRAC_BITS) - 1))
+
 #define itofip(i) ((fip_t)(i) << FIP_FRAC_BITS)
+
 #define fiptoi(fip) ((fip_t)(fip) >> FIP_FRAC_BITS)
+
 // Rounding too
-#define fiptoi_r(fip) (fiptoi(fip) + ((fip_t)(fip) & (1 << (FIP_FRAC_BITS-1))))
+#define fiptoi_r(fip) \
+  ({ fip_t __fip__ = fip; /* Avoid double evaluation */\
+    fiptoi(__fip__) + ((__fip__ & (1 << (FIP_FRAC_BITS-1))) ? 1 : 0); \
+  })
+
 #define fiptof(fip) ((float)(fip) / (1 << FIP_FRAC_BITS))
+
 #define ftofip(f) ((fip_t)((f) * (1 << FIP_FRAC_BITS)))
